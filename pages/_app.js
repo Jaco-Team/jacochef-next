24556c1c991ba0d4571c552b2fc1fc6a90a8c03d
vito3315 <marusiaqwerti@yandex.ru>
2024-11-26 15:49:37 +0400

    111

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,8 @@ import theme from '@/src/theme';
 import '@/styles/global.scss'
 import '@/styles/tender.scss'
 
+import { font } from "@/src/theme";
+
 const Header = dynamic(() => import('@/src/header'), { ssr: false })
 
 export default function MyApp(props) {
@@ -32,11 +34,17 @@ export default function MyApp(props) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="icon" href="/images/favicon.ico" sizes="any" />
+
+        <script src="https://api-maps.yandex.ru/2.1/?apikey=665f5b53-8905-4934-9502-4a6a7b06a900&lang=ru_RU" type="text/javascript"></script>
+        <script src="https://yastatic.net/s3/mapsapi-jslibs/heatmap/0.0.1/heatmap.min.js" type="text/javascript"></script>
+        <link href="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone.css" rel="stylesheet" type="text/css" />
       </Head>
       <ThemeProvider theme={theme}>
+        
         <Header isHeader={isHeader} suppressHydrationWarning />
         
         <Component {...pageProps} />
+        
       </ThemeProvider>
     </AppRouterCacheProvider>
   );
