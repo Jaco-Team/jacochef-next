d7e445e35f3cb2953a16e41c5a5f964e215c4991
vito3315 <marusiaqwerti@yandex.ru>
2025-05-10 13:58:34 +0400

    111

--- pages/overview_modules.js
+++ pages/overview_modules.js
@@ -484,7 +484,7 @@ class OverviewModules_ extends React.Component {
       is_load: true,
     });
 
-    let res = api_laravel_local(this.state.module, method, data)
+    let res = api_laravel(this.state.module, method, data)
       .then(result => result.data)
       .finally( () => {
         setTimeout(() => {
