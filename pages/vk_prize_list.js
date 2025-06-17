deb0d822fec275820aa201dcfeb8de460a19a3f2
MaksSolder <fak8668@gmail.com>
2025-06-17 17:23:30 +0300

    FINAL-34213: Список победителей.

---
+++ pages/vk_prize_list.js
@@ -0,0 +1,221 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+
+
+import {formatDate, MyDatePickerNew} from '@/ui/elements';
+
+import { api_laravel_local, api_laravel } from '@/src/api_new';
+import Button from "@mui/material/Button";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import Table from "@mui/material/Table";
+import Stack from "@mui/material/Stack";
+import {Pagination} from "@mui/lab";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>{children}</Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+class VkPrizeList_ extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      module: 'vk_prize_list',
+      module_name: '',
+      is_load: false,
+      dateFrom: formatDate(new Date()),
+      dateTo: formatDate(new Date()),
+      items: [],
+      points: [],
+      point_id: '',
+      currentPage: 1,
+      itemsPerPage: 10,
+    };
+  }
+
+  async componentDidMount(){
+    let res = await this.getData('get_all');
+
+    this.setState({
+      module_name: res.module_info.name,
+      points: res.points,
+      point_id: res.points[0]['id']
+    })
+
+    document.title = res.module_info.name;
+  }
+
+  getData = (method, data = {}) => {
+
+    this.setState({
+      is_load: true,
+    });
+
+    let res = api_laravel(this.state.module, method, data)
+      .then(result => result.data)
+      .finally( () => {
+        setTimeout(() => {
+          this.setState({
+            is_load: false,
+          });
+        }, 500);
+      });
+
+    return res;
+  }
+
+  getList = async () => {
+    const {dateFrom, dateTo} = this.state;
+    const data = {
+      start_date: dateFrom.toISOString().split('T')[0],
+      end_date: dateTo.toISOString().split('T')[0]
+    };
+    let res = await this.getData('get_data', data);
+    this.setState({
+      items: res.all_items
+    });
+  }
+
+  changeDate = async (e, state) => {
+    this.setState({
+      [state]: formatDate(e)
+    })
+  }
+
+  handlePageChange = (event, page) => {
+    this.setState({
+      currentPage: page,
+    });
+  };
+
+
+  render(){
+    const { items, currentPage, itemsPerPage } = this.state;
+
+    // Вычисляем элементы для текущей страницы
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+    return (
+      <>
+        <Backdrop style={{ zIndex: 99 }} open={this.state.is_load}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+
+        <Grid container spacing={3} className='container_first_child'>
+          <Grid item xs={12} sm={12}>
+            <h1>{this.state.module_name}</h1>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <MyDatePickerNew
+              label="Дата от"
+              value={this.state.dateFrom}
+              func={(e) => this.changeDate(e, 'dateFrom')}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <MyDatePickerNew
+              label="Дата до"
+              value={this.state.dateTo}
+              func={(e) => this.changeDate(e, 'dateTo')}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <Button variant="contained" onClick={this.getList}>Показать</Button>
+          </Grid>
+          <Grid item xs={12} sm={12}>
+
+          </Grid>
+          <Grid item xs={12}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell style={{ width: '20%' }}>Имя</TableCell>
+                  <TableCell style={{ width: '70%' }}>Текстовка приза</TableCell>
+                  <TableCell style={{ width: '10%' }}>Статус</TableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {currentItems.map((value, key) =>
+                    <TableRow key={value.id}>
+                      <TableCell style={{ width: '20%' }}>{value.first_name}</TableCell>
+                      <TableCell style={{ width: '70%' }}>{value.prize}</TableCell>
+                      <TableCell style={{ width: '10%' }}>{value.status}</TableCell>
+                    </TableRow>
+                )}
+              </TableBody>
+            </Table>
+            {items.length > itemsPerPage && (
+                <Stack spacing={3} sx={{mt: 3, mb: 12, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                  <Pagination
+                      count={Math.ceil(items.length / itemsPerPage)}
+                      page={currentPage}
+                      onChange={this.handlePageChange}
+                      color="primary"
+                      showFirstButton
+                      showLastButton
+                  />
+                </Stack>
+            )}
+          </Grid>
+        </Grid>
+      </>
+    )
+  }
+}
+
+export default function VkPrizeList() {
+  return (
+    <VkPrizeList_ />
+  );
+}
+
+export async function getServerSideProps({ req, res, query }) {
+  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=3600');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT');
+
+  return {
+    props: {},
+  }
+}
