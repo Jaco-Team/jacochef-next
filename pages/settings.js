fddf56870ad26749c7f326d25bc14f534e7c0988
MaksSolder <fak8668@gmail.com>
2025-06-30 16:29:01 +0300

    FINAL-34403: Новый модуль "Общие настройки".

---
+++ pages/settings.js
@@ -0,0 +1,393 @@
+import React, {useEffect, useState} from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import {api_laravel_local} from '@/src/api_new';
+import Tab from "@mui/material/Tab";
+import Grid from "@mui/material/Grid";
+import TabContext from "@mui/lab/TabContext";
+import Box from "@mui/material/Box";
+import TabPanel from "@mui/lab/TabPanel";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Tabs from "@mui/material/Tabs";
+import {MyAlert} from "@/ui/elements";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Typography from "@mui/material/Typography";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import Accordion from "@mui/material/Accordion";
+
+const PromoCodeForm = ({ mockItems, getData, setIsLoad, setErrStatus, setErrText, setOpenAlert }) => {
+  const [formData, setFormData] = useState({
+    type: '',
+    discountValue: '',
+    products: [],
+    minOrderAmount: '',
+    maxOrderAmount: '',
+    activationLimit: '',
+    daysBeforeIssue: 0,
+    validityDays: '',
+    description: ''
+  });
+
+  const discountOptions = Array.from({ length: 20 }, (_, i) => (i + 1) * 5);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+
+    if (name === 'type') {
+      setFormData(prev => ({
+      ...prev,
+      discountValue: '',
+      products: []
+    }));
+    }
+  };
+
+  useEffect(() => {
+    if (mockItems) {
+      getData('get_promo').then((data) => {
+        setFormData(
+            {
+              type: data.promo.type,
+              discountValue: parseInt(data.promo.discount),
+              products: JSON.parse(data.promo.products_data),
+              minOrderAmount: data.promo.minOrderAmount,
+              maxOrderAmount: data.promo.maxOrderAmount,
+              activationLimit: data.promo.activationLimit,
+              daysBeforeIssue: data.promo.daysBeforeIssue,
+              validityDays: data.promo.validityDays,
+              description: data.promo.description
+            }
+        );
+      })
+    }
+  }, [mockItems])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoad(true);
+    getData('save_promo', formData).then((data) => {
+      if (!data.st) {
+        setErrStatus(data.st);
+        setErrText(data.text);
+        setOpenAlert(true);
+      } else {
+        getData('get_all')
+      }
+    })
+      .finally(() => {
+        setIsLoad(false);
+      });
+  };
+
+  return (
+      <Box component="form" onSubmit={handleSubmit} sx={{mt: 1}}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth size="small">
+              <InputLabel id="discount-type-label">Тип</InputLabel>
+              <Select
+                  labelId="discount-type-label"
+                  id="type"
+                  name="type"
+                  value={formData.type}
+                  label="Тип скидки"
+                  onChange={handleChange}
+                  required
+              >
+                <MenuItem value="percentage">Cкидка</MenuItem>
+                <MenuItem value="product">Добавление товара</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          {formData.type && formData.type !== 'product' && (
+              <Grid item xs={12} sm={6}>
+                <Autocomplete
+                    options={discountOptions}
+                    size="small"
+                    getOptionLabel={(option) => `${option}%`}
+                    value={formData.discountValue}
+                    onChange={(_, newValue) => {
+                      setFormData(prev => ({
+                        ...prev,
+                        discountValue: newValue
+                      }));
+                    }}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Размер скидки"
+                            required
+                        />
+                    )}
+                />
+              </Grid>
+          )}
+
+          {formData.type === 'product' && (
+              <Grid item xs={12} sm={6}>
+                <Autocomplete
+                    multiple
+                    options={mockItems}
+                    getOptionLabel={(option) => option.name}
+                    value={formData.products}
+                    size="small"
+                    onChange={(_, newValue) => {
+                      setFormData(prev => ({
+                        ...prev,
+                        products: newValue
+                      }));
+                    }}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Выберите товары"
+                            placeholder="Начните вводить название"
+                        />
+                    )}
+                />
+              </Grid>
+          )}
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+                fullWidth
+                label="Минимальная сумма заказа"
+                name="minOrderAmount"
+                type="number"
+                size="small"
+                value={formData.minOrderAmount}
+                onChange={handleChange}
+                inputProps={{min: 0}}
+                required
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+                fullWidth
+                label="Максимальная сумма заказа"
+                name="maxOrderAmount"
+                type="number"
+                value={formData.maxOrderAmount}
+                onChange={handleChange}
+                inputProps={{min: 0}}
+                size="small"
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+                fullWidth
+                size="small"
+                label="Количество активаций"
+                name="activationLimit"
+                type="number"
+                value={formData.activationLimit}
+                onChange={handleChange}
+                inputProps={{min: 1}}
+                required
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+                fullWidth
+                label="За сколько дней выписан промокод"
+                name="daysBeforeIssue"
+                size="small"
+                type="number"
+                value={formData.daysBeforeIssue}
+                onChange={handleChange}
+                inputProps={{min: 0}}
+                required
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+                fullWidth
+                size="small"
+                label="Сколько дней действует промокод"
+                name="validityDays"
+                type="number"
+                value={formData.validityDays}
+                onChange={handleChange}
+                inputProps={{min: 1}}
+                required
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+                fullWidth
+                label="Описание промокода"
+                name="description"
+                multiline
+                size="small"
+                rows={2}
+                value={formData.description}
+                onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} display='grid'>
+            <Button
+                color="success"
+                variant="contained"
+                type="submit"
+                style={{whiteSpace: 'nowrap', justifySelf: 'flex-end'}}
+            >
+              Сохранить изменения
+            </Button>
+          </Grid>
+
+        </Grid>
+      </Box>
+  );
+};
+
+const SettingsPage = () => {
+  const tabsData = {
+    birth_promo: 'Промокод на день рождения',
+  };
+
+  const [isLoad, setIsLoad] = useState(false);
+  const [value, setValue] = useState('');
+  const [mockItems, setMockItems] = useState([]);
+  const [module, setModule] = useState({});
+  const [openAlert, setOpenAlert] = useState(false);
+  const [errStatus, setErrStatus] = useState(false);
+  const [errText, setErrText] = useState('');
+  const [history, setHistory] = useState([])
+  const [tabs, setTabs] = useState(tabsData);
+
+  useEffect(() => {
+    getData('get_all').then((data) => {
+      document.title = data.module_info.name;
+      setModule(data.module_info);
+      setMockItems(data.items);
+      setHistory(data.history);
+      const tabsCheck = Object.entries(tabsData).filter(([key]) => data.acces[key] === "1");
+      setTabs(Object.fromEntries(tabsCheck));
+    });
+  }, []);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const getData = async (method, data = {}) => {
+    setIsLoad(true);
+
+    try {
+      const result = await api_laravel_local('settings', method, data);
+      return result.data;
+    } finally {
+      setIsLoad(false);
+    }
+  };
+
+  return (
+    <>
+      <Backdrop style={{ zIndex: 99 }} open={isLoad}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <MyAlert
+          isOpen={openAlert}
+          onClose={() => setOpenAlert(false)}
+          status={errStatus}
+          text={errText}
+      />
+      <Grid item xs={12} sm={12} container spacing={3} mb={3} className='container_first_child'>
+        <Grid item xs={12} sm={12}>
+          <h1>{module.name}</h1>
+        </Grid>
+        <Grid item xs={12} sm={12} style={{ paddingBottom: 24 }}>
+          <Paper>
+            <TabContext value={value}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="scrollable"
+                scrollButtons={false}
+              >
+                {Object.entries(tabs).map(([key, value]) => <Tab label={value} value={key} />)}
+              </Tabs>
+            </TabContext>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={12} style={{ paddingTop: 0 }}>
+          <TabContext value={value}>
+            <TabPanel value="birth_promo">
+              <PromoCodeForm
+                mockItems={mockItems}
+                getData={getData}
+                setIsLoad={setIsLoad}
+                setErrStatus={setErrStatus}
+                setErrText={setErrText}
+                setOpenAlert={setOpenAlert}
+              />
+              <Accordion style={{marginTop: '24px', display: history.length ? 'inherit' : 'none'}}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                  <Typography sx={{fontWeight: 'bold'}}>История изменений</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>#</TableCell>
+                        <TableCell>Сотрудник</TableCell>
+                        <TableCell>Должность</TableCell>
+                        <TableCell>Дата</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {history.map((item, k) => (
+                          <TableRow key={k} hover>
+                            <TableCell>{k + 1}</TableCell>
+                            <TableCell>{item.user_name}</TableCell>
+                            <TableCell>{item.app_name}</TableCell>
+                            <TableCell>{item.date}</TableCell>
+                          </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </AccordionDetails>
+              </Accordion>
+            </TabPanel>
+          </TabContext>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default function Settings() {
+  return <SettingsPage />;
+}
+
+export async function getServerSideProps({ req, res, query }) {
+  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=3600');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT');
+
+  return {
+    props: {},
+  };
+}
