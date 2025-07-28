25f21ad9fc37b1b95a36ddc7483ac6776678209f
MaksSolder <fak8668@gmail.com>
2025-07-28 08:44:48 +0300

    FINAL-34991: Правка по отчету форм.

--- pages/feedback_form/[id]/index.js
+++ pages/feedback_form/[id]/index.js
@@ -8,6 +8,273 @@ import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
 import Backdrop from '@mui/material/Backdrop';
 import {api_laravel, api_laravel_local} from '@/src/api_new';
+import {formatDate, MyAutocomplite, MyDatePicker, MyDatePickerNew} from "@/ui/elements";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import DialogContent from "@mui/material/DialogContent";
+import Box from "@mui/material/Box";
+import TableFooter from "@mui/material/TableFooter";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import dayjs from "dayjs";
+import TableContainer from "@mui/material/TableContainer";
+
+const ModalOrder = ({open, onClose, getData, pointId, orderId}) => {
+	const [order, setOrder] = useState({});
+	const [order_items, setOrderItems] = useState([]);
+	useEffect(() => {
+		getData('get_order', {order_id: orderId, point_id: pointId}).then((data) => {
+			setOrder(data.order);
+			setOrderItems(data.order_items);
+		})
+	}, [])
+
+	const renderElementFeed = (element, item) => {
+		switch (element.type) {
+			case 'rating':
+				return (
+					<div style={{marginBottom: 10}}>
+						<Typography variant="h6">Рейтинг</Typography>
+						<Rating
+							value={element.data?.value}
+							size="large"
+							sx={{pointerEvents: 'none', opacity: 0.5, span: {fontSize: '2rem !important'}}}
+						/>
+					</div>
+				);
+			case 'input':
+				return (
+					<div style={{marginBottom: 10}}>
+						<Typography variant="h6">{element.data.title}</Typography>
+						<TextField
+							fullWidth
+							sx={{pointerEvents: 'none', opacity: 0.75}}
+							placeholder={element.data.placeholder}
+							size="small"
+						/>
+					</div>
+				);
+			case 'textarea':
+				return (
+					<div style={{marginBottom: 10}}>
+						<Typography variant="h6">{element.data.title}</Typography>
+						<TextField
+							fullWidth
+							multiline
+							size="small"
+							rows={4}
+							sx={{pointerEvents: 'none', opacity: 0.75}}
+							value={element.data?.value}
+							placeholder={element.data.placeholder}
+						/>
+					</div>
+				);
+			case 'heading':
+				return (
+					<div style={{marginBottom: 10}}>
+						<Typography variant="h4">{element.data.text}</Typography>
+					</div>
+				);
+			case 'checkbox':
+				return (
+					<div style={{marginBottom: 10}}>
+						<FormControlLabel
+							control={<Checkbox checked={element.data?.value}/>}
+							label={element.data.label}
+							sx={{pointerEvents: 'none', opacity: 0.75}}
+							id={element.data.param}
+							size="small"
+						/>
+					</div>
+				);
+			case 'checkboxGroup':
+				return (
+					<div style={{
+						marginBottom: 10,
+						display: element.data.conditions.stars.find((value) => value === 1) || element.data.conditions.products.find((value) => value === item.name) || element.data.conditions.categories.find((value) => value === item.cat_name) ? 'initial' : 'none'
+					}}>
+						<Typography variant="h6">{element.data.title}</Typography>
+						{element.data.checkboxes.map((checkbox) => (
+							<div key={checkbox.id} style={{display: 'flex', alignItems: 'center'}}>
+								<FormControlLabel
+									control={<Checkbox checked={checkbox.value} value={checkbox.value} sx={{
+										pointerEvents: 'none',
+										opacity: 0.75
+									}}/>}
+									label={checkbox.label}
+								/>
+							</div>
+						))}
+					</div>
+				);
+			case 'tagCloud':
+				return (
+					<div style={{marginBottom: 10}}>
+						<Typography variant="h6">Облако тегов</Typography>
+						<div style={{display: 'flex', flexWrap: 'wrap', gap: '5px'}}>
+							{element.data.selectedTags.map((tag) => (
+								<Chip sx={{
+									pointerEvents: 'none',
+									opacity: 0.85
+								}} key={tag} label={tag} color={element.data?.value.includes(tag) ? "primary" : "default"} style={{cursor: "pointer"}}/>
+							))}
+						</div>
+					</div>
+				);
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<Dialog
+			open={open}
+			onClose={onClose}
+			aria-labelledby="alert-dialog-title"
+			aria-describedby="alert-dialog-description"
+			fullWidth={true}
+			maxWidth={'md'}
+			fullScreen={false}
+		>
+			<DialogTitle className="button">
+				<Typography style={{
+					fontWeight: 'bold',
+					alignSelf: 'center'
+				}}>Заказ #{order?.order_id}</Typography>
+				<IconButton onClick={onClose} style={{cursor: 'pointer'}}>
+					<CloseIcon/>
+				</IconButton>
+			</DialogTitle>
+
+			<DialogContent>
+
+				<Grid container spacing={0}>
+					<Grid item xs={12}>
+						<span>{order?.type_order}: {order?.type_order_addr_new}</span>
+					</Grid>
+
+					{parseInt(order?.type_order_) == 1 ?
+						parseInt(order?.fake_dom) == 0 ?
+							<Grid item xs={12}>
+								<b style={{color: 'red', fontWeight: 900}}>Домофон не работает</b>
+							</Grid>
+							:
+							<Grid item xs={12}>
+								<b style={{color: 'green', fontWeight: 900}}>Домофон работает</b>
+							</Grid>
+						:
+						null
+					}
+					<Grid item xs={12}>
+						<span>{order?.time_order_name}: {order?.time_order}</span>
+					</Grid>
+
+					{order?.number?.length > 1 ?
+						<Grid item xs={12}>
+							<b>Телефон: </b>
+							<span>{order?.number}</span>
+						</Grid>
+						:
+						null
+					}
+
+					{order?.delete_reason?.length > 0 ?
+						<Grid item xs={12}><span style={{color: 'red'}}>Удален: {order?.date_time_delete}</span></Grid> : null}
+					{order?.delete_reason?.length > 0 ?
+						<Grid item xs={12}><span style={{color: 'red'}}>{order?.delete_reason}</span></Grid> : null}
+
+					{parseInt(order?.is_preorder) == 1 ? null :
+						<Grid item xs={12}><span>{'Обещали: ' + order?.time_to_client + ' / '}{order?.text_time}{order?.time}</span></Grid>
+					}
+
+					{order?.promo_name == null || order?.promo_name?.length == 0 ? null :
+						<>
+							<Grid item xs={12}>
+								<b>Промокод: </b>
+								<span>{order?.promo_name}</span>
+							</Grid>
+							<Grid item xs={12}>
+								<span className="noSpace">{order?.promo_text}</span>
+							</Grid>
+						</>
+					}
+
+					{order?.comment == null || order?.comment.length == 0 ? null :
+						<Grid item xs={12}>
+							<b>Комментарий: </b>
+							<span>{order?.comment}</span>
+						</Grid>
+					}
+
+					{order?.sdacha == null || parseInt(order?.sdacha) == 0 ? null :
+						<Grid item xs={12}>
+							<b>Сдача: </b>
+							<span>{order?.sdacha}</span>
+						</Grid>
+					}
+
+					<Grid item xs={12}>
+						<b>Сумма заказа: </b>
+						<span>{order?.sum_order} р</span>
+					</Grid>
+
+					{order?.check_pos_drive == null || !order?.check_pos_drive ? null :
+						<Grid item xs={12}>
+							<b>Довоз оформлен: </b>
+							<span>{order?.check_pos_drive?.comment}</span>
+						</Grid>
+					}
+
+					<Grid item xs={12}>
+						<Table size={'small'} style={{marginTop: 15}}>
+							<TableBody>
+								{order_items ? order_items.map((item, key) =>
+									<TableRow key={key}>
+										<TableCell>{item.name}</TableCell>
+										<TableCell>{item.count} шт</TableCell>
+										<TableCell>{item.price} р</TableCell>
+										<TableCell><Box sx={{
+											p: 1,
+											bgcolor: item.form_feed?.length ? 'grey.100' : '',
+											display: item.form_feed?.length ? '' : 'none',
+											borderRadius: 1,
+											border: '1px solid',
+											borderColor: 'grey.300',
+										}}
+										>
+											{Object.entries(item.form_feed).map((data) => data.map((element) => renderElementFeed(element, item)))}
+										</Box></TableCell>
+									</TableRow>
+								) : null}
+							</TableBody>
+							<TableFooter>
+								<TableRow>
+									<TableCell style={{fontWeight: 'bold', color: '#000'}}>Сумма заказа</TableCell>
+									<TableCell></TableCell>
+									<TableCell style={{
+										fontWeight: 'bold',
+										color: '#000'
+									}}>{order?.sum_order} р</TableCell>
+									<TableCell></TableCell>
+								</TableRow>
+							</TableFooter>
+						</Table>
+					</Grid>
+
+				</Grid>
+			</DialogContent>
+		</Dialog>
+	);
+}
 
 function FeedbackPage() {
 	const router = useRouter();
@@ -19,6 +286,20 @@ function FeedbackPage() {
 	const [rating, setRating] = useState(1);
 	const [selectedTags, setSelectedTags] = useState([]);
 	const [active, setActive] = useState(false);
+	const [points, setPoints] = useState([]);
+	const [items, setItems] = useState([]);
+	const [point, setPoint] = useState([]);
+	const [item, setItem] = useState([]);
+	const [dateStart, setDateStart] = useState(null);
+	const [dateEnd, setDateEnd] = useState(null);
+	const [feedbacks, setFeedbacks] = useState([]);
+	const [openOrder, setOpenOrder] = useState(false);
+	const [pointId, setPointId] = useState(null);
+	const [orderId, setOrderId] = useState(null);
+	const tabsData = {
+		view_feed: 'Просмотр отчета',
+	};
+	const [access, setAccess] = useState({});
 
 	useEffect(() => {
 		getData('get_form', {id}).then((data) => {
@@ -26,12 +307,34 @@ function FeedbackPage() {
 			setTitle(data.item.name);
 			setFormData(JSON.parse(data.item.form_data));
 			setActive(data.item.active);
+			const tabsCheck = Object.entries(tabsData).filter(([key]) => data.acces?.[key] === '1');
+			setAccess(Object.fromEntries(tabsCheck));
+			setPoints(data.points);
+			setItems(data.items);
 		});
 	}, []);
+
+	const openOrderModal = (pointId, orderId) => {
+		setPointId(pointId);
+		setOrderId(orderId);
+		setOpenOrder(true);
+	}
 
 	const changeActive = (e) => {
 		setActive(e.target.checked);
 		getData('set_active', {check: e.target.checked, id});
+	}
+
+	const getFeedbacks = () => {
+		getData('get_feedback', {
+			dateStart: dayjs(dateStart).format('YYYY-MM-DD'),
+			dateEnd: dayjs(dateEnd).format('YYYY-MM-DD'),
+			point,
+			item,
+			form_id: id
+		}).then((data) => {
+			setFeedbacks(data.feedback);
+		});
 	}
 
 	const toggleTag = (tag) => {
@@ -179,10 +482,89 @@ function FeedbackPage() {
 			<Backdrop style={{zIndex: 99}} open={isLoad}>
 				<CircularProgress color="inherit"/>
 			</Backdrop>
-			{console.log(formData)}
+			{openOrder ? (
+				<ModalOrder open={openOrder} onClose={() => setOpenOrder(false)} getData={getData} orderId={orderId} pointId={pointId}/>) : null}
 			<Grid item xs={12} sm={12}>
 				<h1>{title}</h1>
 			</Grid>
+			{access.view_feed ? (
+				<>
+					<Grid item xs={12} sm={3}>
+						<MyAutocomplite label="Точки" data={points} multiple={true} value={point} func={(event, data) => {
+							setPoint(data)
+						}}/>
+					</Grid>
+					<Grid item xs={12} sm={3}>
+						<MyAutocomplite label="Позиции" data={items} multiple={true} value={item} func={(event, data) => {
+							setItem(data)
+						}}/>
+					</Grid>
+					<Grid item xs={12} sm={3}>
+						<MyDatePickerNew
+							label="Дата начала"
+							value={dateStart}
+							func={(e) => setDateStart(formatDate(e))}
+						/>
+					</Grid>
+					<Grid item xs={12} sm={3}>
+						<MyDatePickerNew
+							label="Дата окончания"
+							value={dateEnd}
+							func={(e) => setDateEnd(formatDate(e))}
+						/>
+					</Grid>
+					<Grid item xs={12} sm={12}>
+						<Button
+							variant="contained"
+							color="primary"
+							onClick={getFeedbacks}
+						>
+							Показать отчет
+						</Button>
+					</Grid>
+					{feedbacks.length ? (
+						<Grid item xs={12} sm={12}>
+							<Accordion>
+								<AccordionSummary expandIcon={<ExpandMoreIcon/>} aria-controls="panel1a-content">
+									<Typography style={{whiteSpace: 'nowrap', fontWeight: 'bold '}}> Отзывы ({feedbacks.length}) </Typography>
+								</AccordionSummary>
+								<AccordionDetails>
+									<Accordion>
+										<TableContainer>
+											<Table>
+												<TableHead>
+													<TableRow>
+														<TableCell>#</TableCell>
+														<TableCell>Номер заказа</TableCell>
+														<TableCell>Точка</TableCell>
+														<TableCell>Отзывов по товару</TableCell>
+														<TableCell>Отзывов по категории</TableCell>
+														<TableCell>Отзывов по типу заказа</TableCell>
+														<TableCell>Дата отзыва</TableCell>
+													</TableRow>
+												</TableHead>
+												<TableBody>
+													{feedbacks.map((it, k) => (
+														<TableRow style={{cursor: 'pointer'}} hover key={it.order_id} onClick={() => openOrderModal(it.point_id, it.order_id)}>
+															<TableCell>{k + 1}</TableCell>
+															<TableCell>{it.order_id}</TableCell>
+															<TableCell>{it.point_name}</TableCell>
+															<TableCell>{it.count_things}</TableCell>
+															<TableCell>{it.count_cat}</TableCell>
+															<TableCell>{it.count_order}</TableCell>
+															<TableCell>{it.date}</TableCell>
+														</TableRow>
+													))}
+												</TableBody>
+											</Table>
+										</TableContainer>
+									</Accordion>
+								</AccordionDetails>
+							</Accordion>
+						</Grid>
+					) : null}
+				</>
+			) : null}
 			<Grid item xs={12} sm={12}>
 				<FormControlLabel
 					sx={{pt: 0}}
