d8eebc53473445b8f4f5cf6d93bb71bd64d1db6f
tedgregory <mail@tedgregory.ru>
2025-07-29 18:33:02 +0300

    fix: api url

--- src/api_new.js
+++ src/api_new.js
@@ -44,7 +44,7 @@ export function api(module = '', method = '', data = {}, dop_type = {}) {
 }
 
 export function api_laravel(module = '', method = '', data = {}, dop_type = {}) {
-  //const urlApi_dev = 'http://127.0.0.1:8000/api/'+module+'/'+method;
+  // const urlApi_dev = 'http://127.0.0.1:8000/api/'+module+'/'+method;
   //const urlApi_dev = 'https://79.174.91.113/api/'+module+'/'+method;
   const urlApi_dev = 'https://apichef.jacochef.ru/api/'+module+'/'+method;
 
