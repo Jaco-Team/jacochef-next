4b93508876d2352315dfc21900e922fd68bda4d1
vito3315 <marusiaqwerti@yandex.ru>
2025-08-13 13:16:40 +0400

    111

--- pages/site_clients/index.js
+++ pages/site_clients/index.js
@@ -66,6 +66,7 @@ import {useRouter} from "next/router";
 import DownloadIcon from "@mui/icons-material/Download";
 import TablePagination from "@mui/material/TablePagination";
 import {Checkbox, Chip, FormControlLabel, Rating, TextField} from "@mui/material";
+
 import SiteClients_Modal_Comment_Action from "@/pages/site_clients/_SiteClientsModalCommentAction";
 import SiteClients_Modal_Client_Order from "@/pages/site_clients/_SiteClientsModalClientOrder";
 import SiteClients_Modal_Client from "@/pages/site_clients/_SiteClientsModalClient";
--- pages/site_setting/components/banners/SiteSettingBanners.jsx
+++ pages/site_setting/components/banners/SiteSettingBanners.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { MyCheckBox, MySelect, MyTextInput } from "@/ui/elements";
 import {
   Accordion,
