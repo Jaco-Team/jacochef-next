f2bc1e74a84e4383bf76cb0854f2261a8095ddfc
tedgregory <mail@tedgregory.ru>
2025-08-25 19:26:05 +0300

    feat: [35619] show_cook new api, frontend update
    feat: set up new api, lint
    
    refactor: decompose components
    
    Update image handling in CookCatsTab and CookItemModal
    
    Refactored image rendering logic in CookCatsTab and CookItemModal to use direct <img> and <picture> tags instead of Avatar and Image components. Adjusted image URLs and sizes for consistency. Also switched API call in show_cook.js from api_laravel to api_laravel_local for data fetching.
    
    feat: add modal close button
    
    fix: api connector back to prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-import { getGlobals } from 'common-es'
-const { __dirname, __filename } = getGlobals(import.meta.url)
+import { getGlobals } from "common-es";
+const { __dirname, __filename } = getGlobals(import.meta.url);
 
 const nextConfig = {
-    sassOptions: {
-        fiber: false,
-    },
+  sassOptions: {
+    fiber: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.yandexcloud.net",
+        pathname: "/site-img/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
