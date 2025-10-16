bbcaa0879df420a7fe32a4709cfbd631c8861051
vito3315 <marusiaqwerti@yandex.ru>
2025-10-16 23:38:23 +0400

    111

--- pages/cafe_edit.js
+++ pages/cafe_edit.js
@@ -10,38 +10,38 @@ export default function CafeEditPage({ initialData, initialPointData }) {
   );
 }
 
-export async function getServerSideProps({ req, res }) {
-  res.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=3600");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
+// export async function getServerSideProps({ req, res }) {
+//   res.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=3600");
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
 
-  const initial = { initialData: null, initialPointData: null };
-  try {
-    const data = await getDataSSR("cafe_edit", "get_all", req.headers.cookie);
-    if (data?.redirect) {
-      return data;
-    }
-    initial.initialData = data?.data || null;
-    if (data?.data?.points?.length) {
-      const point = data.data.points[0];
-      const pointData = await getDataSSR("cafe_edit", "get_one", req.headers.cookie, {
-        point_id: point.id,
-        city_id: point.city_id,
-      });
-      if (pointData?.redirect) {
-        return pointData;
-      }
-      initial.initialPointData = pointData?.data || null;
-    }
-    return {
-      props: initial,
-    };
-  } catch (e) {
-    console.error(e);
-  }
-}
+//   const initial = { initialData: null, initialPointData: null };
+//   try {
+//     const data = await getDataSSR("cafe_edit", "get_all", req.headers.cookie);
+//     if (data?.redirect) {
+//       return data;
+//     }
+//     initial.initialData = data?.data || null;
+//     if (data?.data?.points?.length) {
+//       const point = data.data.points[0];
+//       const pointData = await getDataSSR("cafe_edit", "get_one", req.headers.cookie, {
+//         point_id: point.id,
+//         city_id: point.city_id,
+//       });
+//       if (pointData?.redirect) {
+//         return pointData;
+//       }
+//       initial.initialPointData = pointData?.data || null;
+//     }
+//     return {
+//       props: initial,
+//     };
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
