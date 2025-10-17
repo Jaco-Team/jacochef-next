cadd42a7f93e77753a76d7718c9702482d43c0ab
vito3315 <marusiaqwerti@yandex.ru>
2025-10-17 11:24:11 +0400

    111

--- components/shared/Forms/MySelect.jsx
+++ components/shared/Forms/MySelect.jsx
@@ -1,74 +1,74 @@
-"use client";
+// "use client";
 
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+// import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
-export function MySelect(props) {
-  const { data = [], multiple, is_none = true } = props;
+// export function MySelect(props) {
+//   const { data = [], multiple, is_none = true } = props;
 
-  // все id -> строки
-  const items = data.map((i) => ({ ...i, id: String(i.id) }));
+//   // все id -> строки
+//   const items = data.map((i) => ({ ...i, id: String(i.id) }));
 
-  // значение -> строка (или "" для пустого) / массив строк для multiple
-  const normalizedValue = multiple
-    ? Array.isArray(props.value) ? props.value.map(String) : []
-    : props.value == null ? "" : String(props.value);
+//   // значение -> строка (или "" для пустого) / массив строк для multiple
+//   const normalizedValue = multiple
+//     ? Array.isArray(props.value) ? props.value.map(String) : []
+//     : props.value == null ? "" : String(props.value);
 
-  // нормализуем event и пробрасываем его вверх
-  const handleChange = (muiEvent) => {
-    console.log('handleChange', muiEvent.target.value)
-    muiEvent.persist?.(); // на всякий случай для iOS/React 18
-    const next = muiEvent.target.value;
+//   // нормализуем event и пробрасываем его вверх
+//   const handleChange = (muiEvent) => {
+//     console.log('handleChange', muiEvent.target.value)
+//     muiEvent.persist?.(); // на всякий случай для iOS/React 18
+//     const next = muiEvent.target.value;
 
-    // гарантируем строку/массив строк
-    const normalized =
-      Array.isArray(next) ? next.map(String) : String(next);
+//     // гарантируем строку/массив строк
+//     const normalized =
+//       Array.isArray(next) ? next.map(String) : String(next);
 
-    // склеим «как нативный» и отдадим наверх
-    const customEvent = {
-      ...muiEvent,
-      target: {
-        ...muiEvent.target,
-        value: normalized,
-      },
-    };
-    props.func && props.func(customEvent);
-  };
+//     // склеим «как нативный» и отдадим наверх
+//     const customEvent = {
+//       ...muiEvent,
+//       target: {
+//         ...muiEvent.target,
+//         value: normalized,
+//       },
+//     };
+//     props.func && props.func(customEvent);
+//   };
 
-  const labelId = "my-select-label";
-  const selectId = "my-select";
+//   const labelId = "my-select-label";
+//   const selectId = "my-select";
 
-  console.log('value', props.value, normalizedValue)
-  console.log('items', items)
+//   console.log('value', props.value, normalizedValue)
+//   console.log('items', items)
 
-  return (
-    <FormControl fullWidth variant="outlined" size="small" style={props.style}>
-      {props.label && <InputLabel >{props.label}</InputLabel>}
-      <Select
-        value={normalizedValue}              
-        label={props.label}
-        //disabled={!!props.disabled}
-        multiple={!!multiple}
-        onChange={handleChange}
-      >
-        {is_none && !multiple && (
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-        )}
+//   return (
+//     <FormControl fullWidth variant="outlined" size="small" style={props.style}>
+//       {props.label && <InputLabel >{props.label}</InputLabel>}
+//       <Select
+//         value={normalizedValue}              
+//         label={props.label}
+//         //disabled={!!props.disabled}
+//         multiple={!!multiple}
+//         onChange={handleChange}
+//       >
+//         {is_none && !multiple && (
+//           <MenuItem value="">
+//             <em>None</em>
+//           </MenuItem>
+//         )}
 
-        {items.map((item) => (
-          <MenuItem
-            key={item.id}
-            value={item.id}                   
-            style={{ color: item.color ?? undefined, zIndex: 9999 }}
-          >
-            {item.name}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
-  );
-}
+//         {items.map((item) => (
+//           <MenuItem
+//             key={item.id}
+//             value={item.id}                   
+//             style={{ color: item.color ?? undefined, zIndex: 9999 }}
+//           >
+//             {item.name}
+//           </MenuItem>
+//         ))}
+//       </Select>
+//     </FormControl>
+//   );
+// }
 
 // "use client";
 
@@ -142,35 +142,38 @@ export function MySelect(props) {
 //   );
 // }
 
-// export class MySelect extends React.PureComponent {
-//   constructor(props) {
-//     super(props);
+import React from "react";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
-//     this.state = {
-//     };
-//   }
+export class MySelect extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-//   render(){
-//     return (
-//       <FormControl fullWidth variant="outlined" size="small" style={this.props.style}>
-//         <InputLabel>{this.props.label}</InputLabel>
-//         <Select
-//           value={this.props.value}
-//           label={this.props.label}
-//           disabled={ this.props.disabled || this.props.disabled === true ? true : false }
-//           onChange={ this.props.func }
-//           multiple={ this.props.multiple && this.props.multiple === true ? true : false }
-//           //style={{ zIndex: 9999 }}
-//         >
-//           {this.props.is_none === false ? null :
-//             <MenuItem value=""><em>None</em></MenuItem>
-//           }
+    this.state = {
+    };
+  }
 
-//           { this.props.data?.map( (item, key) =>
-//             <MenuItem key={key} value={item.id} style={{ color: item?.color ? item.color : null, zIndex: 9999 }}>{item.name}</MenuItem>
-//           ) }
-//         </Select>
-//       </FormControl>
-//     )
-//   }
-// }
+  render(){
+    return (
+      <FormControl fullWidth variant="outlined" size="small" style={this.props.style}>
+        <InputLabel>{this.props.label}</InputLabel>
+        <Select
+          value={this.props.value}
+          label={this.props.label}
+          disabled={ this.props.disabled || this.props.disabled === true ? true : false }
+          onChange={ this.props.func }
+          multiple={ this.props.multiple && this.props.multiple === true ? true : false }
+          //style={{ zIndex: 9999 }}
+        >
+          {this.props.is_none === false ? null :
+            <MenuItem value=""><em>None</em></MenuItem>
+          }
+
+          { this.props.data?.map( (item, key) =>
+            <MenuItem key={key} value={item.id} style={{ color: item?.color ? item.color : null, zIndex: 9999 }}>{item.name}</MenuItem>
+          ) }
+        </Select>
+      </FormControl>
+    )
+  }
+}
