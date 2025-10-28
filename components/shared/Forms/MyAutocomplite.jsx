b67517c2d5e781b556c131f90400b91565a88637
MaksSolder <fak8668@gmail.com>
2025-10-28 13:49:21 +0300

    FINAL-35587: Новые Товары склада, Рецепты и заготовки.

--- components/shared/Forms/MyAutocomplite.jsx
+++ components/shared/Forms/MyAutocomplite.jsx
@@ -19,6 +19,8 @@ export function MyAutocomplite(props) {
           getOptionKey={props.getOptionKey}
           value={props.value}
           onChange={props.func}
+          autoFocus={props.autoFocus}
+    			disableAutoFocus={props.disableAutoFocus}
           onBlur={props.onBlur || undefined}
           filterSelectedOptions
           multiple={props.multiple && props.multiple === true ? true : false}
