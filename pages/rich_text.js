9158ef16e546632c7c6b78cea5814af89d99a745
tedgregory <mail@tedgregory.ru>
2025-10-30 14:05:21 +0300

    draft a playground for ck and jodit

--- pages/rich_text.js
+++ pages/rich_text.js
@@ -1,17 +1,20 @@
 // import { TextEditor } from "@/components/rich_text/LexKit";
 // import { TexetEditor } from "@/components/rich_text/TipTap";
 // import TextEditor from "@/components/rich_text/CK";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import dynamic from "next/dynamic";
 import { useRef, useState } from "react";
+import fs from "fs";
 
-// const TextEditor = dynamic(() => import("@/components/rich_text/CK"), { ssr: false });
+const defaultText = fs.readFileSync("@/components/rich_text/README.md", "utf-8");
+
+const TextEditor1 = dynamic(() => import("@/components/rich_text/CK"), { ssr: false });
 // const TextEditor = dynamic(() => import('@/components/rich_text/TipTap'), { ssr: false });
 // const TextEditor = dynamic(() => import('@/components/rich_text/LexKit'), { ssr: false });
 // const TextEditor = dynamic(() => import("@/components/rich_text/BlockNote"), { ssr: false });
 const TextEditor = dynamic(() => import("@/components/rich_text/Jodit"), { ssr: false });
 export default function WysiwygEditor() {
-  const [content, setContent] = useState("<p>Hello world!</p>");
+  const [content, setContent] = useState(defaultText);
   const editorRef = useRef(null);
   return (
     <Grid
@@ -28,7 +31,20 @@ export default function WysiwygEditor() {
           menubar={false} // unused but kept for parity
           language="ru"
         /> */}
+      </Grid>
+      <Grid size={12}>
+        <Typography variant="h6">Jodit</Typography>
         <TextEditor
+          value={content}
+          onChange={setContent}
+          toolbar
+          menubar
+          language="ru"
+        />
+      </Grid>
+      <Grid size={12}>
+        <Typography variant="h6">CKEditor</Typography>
+        <TextEditor1
           value={content}
           onChange={setContent}
           toolbar
